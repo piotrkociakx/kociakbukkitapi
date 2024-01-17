3360f8f5012981d1d37c8a83c8f7c290958bf372
piotrkociakx <piotrekstypczynski@gmail.com>
2024-01-17 12:22:04 +0100

    kociakbukkitapi v1.1

--- src/main/java/pl/Author/kociakbukkitapi/methods/plugin/PluginMethods.java
+++ src/main/java/pl/Author/kociakbukkitapi/methods/plugin/PluginMethods.java
@@ -5,7 +5,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.Author.Main.Main;
+import pl.Author.kociakbukkitapi.Kociak_bukkit_api;
 import pl.Author.kociakbukkitapi.commandHandler.CommandKittyHandler;
+import pl.Author.kociakbukkitapi.methods.player.apiPlayer;
 
 import java.util.List;
 
@@ -16,31 +18,21 @@ public class PluginMethods {
 
     public PluginMethods() {
         this.enabletabcompleter = true;
-<<<<<<< Updated upstream
-        this.main = Main.instance;
         this.plugin = Main.instance;
-=======
-        this.plugin = Main.getInstance();
->>>>>>> Stashed changes
     }
     public void registerPermission(String permission) {
         Permission finalpermission = new Permission(permission);
         plugin.getServer().getPluginManager().addPermission(finalpermission);
     }
-    public void addTabCompleter(String command, List<String> option1, List<String> option2, List<String> option3, List<String> option4, List<String> option5) {
-        plugin.getCommand(command).setTabCompleter(new TabCompleterImplementation(command, option1, option2, option3, option4, option5));
+    public void addTabCompleter(String command, List<List<String>> options) {
+        plugin.getCommand(command).setTabCompleter(new TabCompleterImplementation(command, options));
     }
+
     public void registerCommand(String commandName, CommandKittyHandler.CommandHandler commandHandler) {
-<<<<<<< Updated upstream
-        PluginCommand command = main.getCommand(commandName);
-        if (command != null) {
-            command.setExecutor(new CommandKittyHandler(commandHandler, Main.instance.configManager, main));
-=======
         PluginCommand command = plugin.getCommand(commandName);
 
         if (command != null) {
             command.setExecutor(new CommandKittyHandler(commandHandler, Main.getInstance().configManager, plugin));
->>>>>>> Stashed changes
         } else {
             plugin.getLogger().warning("Plugin '" + plugin.getName() + "' can't load a command '" + commandName + "', maybe is not registered in plugin.yml?");
         }
